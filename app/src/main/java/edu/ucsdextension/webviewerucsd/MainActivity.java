c07798d73a26535ac0677c64be902a31c4e98cb4
Your Name <you@example.com>
2017-05-30 00:30:18 -0700

    Added comment in the main activity class

--- app/src/main/java/edu/ucsdextension/webviewerucsd/MainActivity.java
+++ app/src/main/java/edu/ucsdextension/webviewerucsd/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/*This is main activity
+
+ */
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonYahoo, buttonUCSD;
